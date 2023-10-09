88a523e4315ca000457f48ba0368833e740c7595
dorakurt <muratkurt---@hotmail.com>
2023-10-09 18:59:18 +0300

    rev21

---
+++ Java101/src/Kombinasyon_Hesaplayan_Program.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Kombinasyon_Hesaplayan_Program {
+    public static void main(String[]args){
+        int n,r,nfak = 1, rfak = 1, C,nrfak = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman sayısını giriniz (n) : ");
+        n = input.nextInt();
+        System.out.print("Seçim sayısını giriniz (r) : ");
+        r = input.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            nfak = nfak * i;
+        }
+        for (int i = 1; i <= r; i++){
+            rfak = rfak * i;
+        }
+        for (int i = 1; i <= n - r; i++){
+            nrfak = nrfak * i;
+        }
+        int sonuc = nfak / (rfak*nrfak);
+        System.out.println("C(" + n + "," + r + ") = " + sonuc);
+
+    }
+}
